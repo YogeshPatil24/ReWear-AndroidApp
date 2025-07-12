47d17c3e831ef1d23924a9feabdbd6e22454abe9
Yogesh <yogeshpatilbmp@gmail.com>
2025-07-12 10:57:46 +0530

    mainactivity logs added.

--- app/src/main/java/com/android/rewear/MainActivity.kt
+++ app/src/main/java/com/android/rewear/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.android.rewear
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("DONEE", "onCreate: code commit hisotru.")
     }
 }
